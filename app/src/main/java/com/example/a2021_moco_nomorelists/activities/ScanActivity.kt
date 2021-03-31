dea121f779d545f5135bd31cae2bf8cfb7395e1c
JackR1p <43586753+JackR1p@users.noreply.github.com>
2021-03-31 13:19:59 +0200

    CodeScanner mit dependency, manifest zur Erlabnis auf Zuriff auf die Kamera und die Scannapp mit layout xml

--- app/src/main/java/com/example/a2021_moco_nomorelists/activities/ScanActivity.kt
+++ app/src/main/java/com/example/a2021_moco_nomorelists/activities/ScanActivity.kt
@@ -1,12 +1,63 @@
 package com.example.a2021_moco_nomorelists.activities
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.budiyev.android.codescanner.*
 import com.example.a2021_moco_nomorelists.R
 
+private const val CAMERA_REQUEST_CODE = 101
+
 class ScanActivity : AppCompatActivity() {
+
+    private lateinit var codeScanner: CodeScanner
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scan)
+
+
     }
+
+    private fun codeScanner() {
+        codeScanner = CodeScanner(this, scanner_view)
+
+        codeScanner.apply{
+            camera = CodeScanner.CAMERA_BACK
+            formats = CodeScanner.ALL_FORMATS
+
+            autoFocusMode = AutoFocusMode.SAFE
+            scanMode = ScanMode.CONTINUOUS
+            isAutoFocusEnabled = true
+            isFlashEnabled = false
+
+            decodeCallback = DecodeCallback{
+                runOnUiThread{
+                    tv_textView.text = it.text //Decodiert Information zum Scan
+
+                }
+            }
+
+            errorCallback = ErrorCallback{
+                runOnUiThread{
+                    Log.e("Main", "Kamera konnte nicht initialisiert werden: ${it.message}") //Error Message bei Scanfehler
+                }
+            }
+        }
+
+        scanner_view.setOnClickListener {
+            codeScanner.startPreview()
+        }
+    }
+
+    override fun onResume() { //Scannt wieder bei Resume der app
+        super.onResume()
+        codeScanner.startPreview()
+    }
+
+    override fun onPause() {
+        codeScanner.releaseResources() //Hilft gegen Memory leaks
+        super.onPause()
+    }
+
+    
 }
